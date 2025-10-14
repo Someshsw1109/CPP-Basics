b8185034c5c61d7e53d1878e97576eae4e8f2709
Somesh Raj <someshraj78569@gmail.com>
2025-10-14 18:29:39 +0530

    Input/output

--- Input and output/input.cpp
+++ Input and output/input.cpp
@@ -3,10 +3,37 @@
 using namespace std;
 
 int main() {
-    int a;
-    int b;
+    // int a;
+    // int b;
+    // cin >> a >> b;
+    // cout << "value of a is " << a << " and value of b is " << b;
+    // return 0;
+
+    // integer 
+    int a = 10;
+    // long
+    long b = 15;
+
     cin >> a >> b;
-    cout << "value of a is " << a << " and value of b is " << b;
+
+    long long c = 10000000000;
+
+    // float, double
+
+    float d = 2.0;
+
+    float e = 5;
+
+    cin >> d >> e;
+
+
+    cout << "Value of a is " << a << endl;
+    cout << "Value of b is " << b << endl;
+    cout << "Value of c is " << c << endl;
+    cout << fixed << setprecision(1); // Use I/O manipulators from <iomanip>:
+    cout << "Value of d is " << d << endl;
+    cout << "Value of e is " << e << endl;
     return 0;
+
 }
 
