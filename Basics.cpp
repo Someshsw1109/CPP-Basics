2b516fb4608f9d620707617ad945c74d9552e178
Somesh Raj <someshraj78569@gmail.com>
2025-10-14 18:06:24 +0530

    First commit

---
+++ Basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+// #include<math.h>
+// #include<string>
+using namespace std;
+
+
+int main() {
+    std::cout << "Hey Somesh" << std::endl << "Hey Raj" << std::endl;
+    return 0;
+}
+
+// How we print output in cpp
