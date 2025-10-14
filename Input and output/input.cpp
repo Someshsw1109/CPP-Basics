71809ec186606aae1dd3ebea08369d727b9245c4
Somesh Raj <someshraj78569@gmail.com>
2025-10-14 18:17:02 +0530

    Input/Output and basics of cpp

---
+++ Input and output/input.cpp
@@ -0,0 +1,12 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << "value of a is " << a << " and value of b is " << b;
+    return 0;
+}
+
