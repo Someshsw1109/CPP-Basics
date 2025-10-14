64f2172481af4fe5af0db8303d88786f515f99d8
Somesh Raj <someshraj78569@gmail.com>
2025-10-14 20:02:07 +0530

    For Loops

---
+++ For Loops/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    // for intialization; condition; updation
+    for (i = 1; i <= 200; i = i + 1/*we can also write i++ instead of i = i + 1*/) {
+        cout << "Somesh " << i << endl;
+    }
+    cout << i << endl;
+
+    return 0;
+}
+
